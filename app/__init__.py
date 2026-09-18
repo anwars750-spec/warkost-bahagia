9273ce94978b9051eca1d6a553f91c114e07218e
anwars750-spec <anwars750@gmail.com>
2026-09-18 20:38:14 +0700

    Update app factory to allow a deployment-safe database path via environment variable.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .routes import bp
 def create_app():
     app=Flask(__name__,instance_relative_config=True)
     app.config['SECRET_KEY']=os.environ.get('WARKOST_SECRET_KEY','local-dev-change-me')
-    app.config['DATABASE']=app.instance_path+'/warkost.db'
+    app.config['DATABASE']=os.environ.get('WARKOST_DATABASE', app.instance_path+'/warkost.db')
     app.config['MAX_CONTENT_LENGTH']=5*1024*1024
     init_db(app)
     app.register_blueprint(bp)
